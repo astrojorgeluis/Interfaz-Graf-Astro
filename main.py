723f4d333edfdc064de3ab182920afa0e65e5330
Jorge Luis <144159564+astrojorgeluis@users.noreply.github.com>
2023-11-27 23:15:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -61,15 +61,13 @@ else:
     charts = [
         alt.Chart(variables[nombre]).mark_point(filled=False).encode(
             x='Tiempo desde erupcion (d)',
-            y=alt.Y('Magnitud', scale=alt.Scale(domain=(variables[nombre]['Magnitud'].max(), variables[nombre]['Magnitud'].min()))),  # Invertir el eje Y
+            y=alt.Y('Magnitud', scale=alt.Scale(domain=(variables[nombre]['Magnitud'].min(), variables[nombre]['Magnitud'].max()), reverse=True)),  # Invertir el eje Y
             size=alt.value(tamaño_circulos),
             color=alt.Color('Archivo:N', scale=alt.Scale(scheme='plasma')),
             opacity=alt.value(0.5),
             tooltip=['Tiempo desde erupcion (d)', 'Magnitud']
         ).transform_calculate(
             Archivo='"' + nombre + '"'
-        ).transform_filter(
-            alt.FieldOneOfPredicate(field='Archivo', oneOf=seleccionados)
         )
         for nombre in seleccionados
     ]
